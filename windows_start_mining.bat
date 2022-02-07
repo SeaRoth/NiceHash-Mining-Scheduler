243e40e145f72f083562a565579db6b6032b39b9
SeaRoth <coryr32@gmail.com>
2022-02-06 20:36:56 -0800

    updated some locations

--- windows_start_mining.bat
+++ windows_start_mining.bat
@@ -1,2 +1,2 @@
-"C:\Users\coryr\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\coryr\OneDrive\Documents\GitHub\NiceHash-Windows-Scheduler\start_mining.py"
+"C:\Users\coryr\AppData\Local\Programs\Python\Python39\python.exe" "start_mining.py"
 pause
--- windows_stop_mining.bat
+++ windows_stop_mining.bat
@@ -1,2 +1,2 @@
-"C:\Users\coryr\AppData\Local\Programs\Python\Python39\python.exe" "C:\Users\coryr\OneDrive\Documents\GitHub\NiceHash-Windows-Scheduler\stop_mining.py"
+"C:\Users\coryr\AppData\Local\Programs\Python\Python39\python.exe" "stop_mining.py"
 pause
